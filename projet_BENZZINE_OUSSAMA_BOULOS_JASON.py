94624e2406393eb4f95e4a0556f417d7e758a4ce
osobenz1 <benzzine.oussama.ob@gmail.com>
2024-12-07 21:30:00 +0100

    all algorithms implemented

--- projet_BENZZINE_OUSSAMA_BOULOS_JASON.py
+++ projet_BENZZINE_OUSSAMA_BOULOS_JASON.py
@@ -100,3 +100,60 @@ def executerTri(fct_tri, color, nom, nlist=15, nval=200, surplace = True):
     plt.title(f"Performance de l'algorithme de tri \"{nom}\"")  # Titre du graphique
     plt.legend()
     plt.show()
+
+def executerTri_quick_sort(fct_tri, color, nom, nlist=15, nval=200, surplace=True):
+    axis, listDataRandom, listDataSorted, listDataInvertedSorted = create_data(nlist, nval)
+
+    toplotRandom = []
+    toplotSorted = []
+    toplotInverted = []
+
+    dataTestRandom = copy.deepcopy(listDataRandom)
+    dataTestSorted = copy.deepcopy(listDataSorted)
+    dataTestInverted = copy.deepcopy(listDataInvertedSorted)
+
+    for i in range(len(axis)):
+        time1 = time.time()
+        if surplace:
+            fct_tri(dataTestRandom[i], 0, len(dataTestRandom[i])-1)
+        else:
+            fct_tri(dataTestRandom[i], 0, len(dataTestRandom[i])-1)
+        time2 = time.time()
+        toplotRandom.append((time2 - time1) * 1000)
+        time3 = time.time()
+        if surplace:
+            fct_tri(dataTestSorted[i], 0, len(dataTestSorted[i])-1)
+        else:
+            fct_tri(dataTestSorted[i], 0, len(dataTestSorted[i])-1)
+        time4 = time.time()
+        toplotSorted.append((time4 - time3) * 1000)
+        time5 = time.time()
+        if surplace:
+            fct_tri(dataTestInverted[i], 0, len(dataTestInverted[i])-1)
+        else:
+            fct_tri(dataTestInverted[i], 0, len(dataTestInverted[i])-1)
+        time6 = time.time()
+        toplotInverted.append((time6 - time5) * 1000)
+
+    (ok1, ipb1) = areSorted(dataTestRandom)
+    (ok2, ipb2) = areSorted(dataTestSorted)
+    (ok3, ipb3) = areSorted(dataTestInverted)
+
+    if not ok1:
+        print(nom + ' data random incorrect, liste #' + str(ipb1))
+    else:
+        plt.plot(axis, toplotRandom, '-' + color, label=nom + ' (Cas Moyen)')
+    if not ok2:
+        print(nom + ' data Sorted incorrect, liste #' + str(ipb2))
+    else:
+        plt.plot(axis, toplotSorted, '--' + color, label=nom + ' (Meilleur Cas)')
+
+    if not ok3:
+        print(nom + ' data Inverted incorrect, liste #' + str(ipb3))
+    else:
+        plt.plot(axis, toplotInverted, ':' + color, label=nom + ' (Pire Cas)')
+    plt.xlabel("Taille du tableau")  # Titre de l'axe x
+    plt.ylabel("Temps d'exécution (millisecondes)")  # Titre de l'axe y
+    plt.title(f"Performance de l'algorithme de tri \"{nom}\"")  # Titre du graphique
+    plt.legend()
+    plt.show()
